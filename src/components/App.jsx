274656fbd7ca4e4b2cb16b30785274e78ec64c29
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-04-18 21:47:52 +0200

    zad 2 cz1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { Feedback } from "./feedback";
+import { Statistics } from "./Statistics";
+import React, { useState } from 'react';
+
+
 export const App = () => {
+
+  const [ good, setGood ]= useState(0)
+  const [ neutral, setNeutral ]= useState(0)
+  const [bad, setBad] = useState(0)
+  const [value, setValue] = useState(0)
+  const [result] = useState(0)
+  
+
+    const onClickGood = () => {
+      setGood(prevValue => prevValue + 1)
+    }  
+    
+    const onClicNeutral = () => {
+        setNeutral(prevValue =>prevValue+1)
+    } 
+    
+    const onClicBad = () => {
+        setBad(prevValue =>prevValue+1)
+    }  
+    
+    const countTotalFeedback=() => {
+      setValue(prevValue => prevValue + 1)
+  }
+  
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <Feedback  good={good} value={value} result={result} onClickGood={onClickGood} onClicNeutral={onClicNeutral} onClicBad={onClicBad} totalClick={countTotalFeedback} />
+    <Statistics good={good} neutral={neutral} bad={bad} value={value} result={result} />
+    </>
   );
 };
